1d50379a89bf2caa71633e97a35c33e29fa11275
Duribowka <s.boronin08@gmail.com>
2025-11-05 18:59:30 +0000

    Dinamical allocation for things

--- headers/structs.h
+++ headers/structs.h
@@ -1,36 +1,31 @@
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
-//NEEDS CHECKING
-
-typedef struct{
+typedef struct {
     int day;
     int month;
     int year;
-}Date;
+} Date;
 
-struct stocking{
-    char name[50];
-    char type[50];
+struct stocking {
+    char *name;
+    char *type;
     Date date;
     int weight;
     float price;
     float width;
     float height;
-    char author[50];
+    char *author;
     int stock;
 };
 
-struct author{
-    char name[50];
-    char street[50];
-    char mail[50];
-    char website[50];
-    char phone[50];
-    char country[50];
+struct author {
+    char *name;
+    char *street;
+    char *mail;
+    char *website;
+    char *phone;
+    char *country;
     Date birthdate;
 };
 
--- main.c
+++ main.c
@@ -53,16 +53,38 @@ void print_entry(const char *filename, const char *label, const char *target){
 int main(int argc, char **argv){
     struct stocking *s = malloc(sizeof(struct stocking));
     struct author *a = malloc(sizeof(struct author));
+
+    s->name = malloc(100);
+    s->type = malloc(100);
+    s->author = malloc(100);
+
+    a->name = malloc(100);
+    a->street = malloc(100);
+    a->mail = malloc(100);
+    a->website = malloc(100);
+    a->phone = malloc(100);
+    a->country = malloc(100);
+
     int isAuthor = 0;
 
     for (int i = 1; i < argc; i++) {
         if (mystrcmp(argv[i], "--author") == 0) isAuthor = 1;
         else if (mystrcmp(argv[i], "--item") == 0) isAuthor = 0;
 
-        if (mystrcmp(argv[i], "-name") == 0)
-            mystrcpy(isAuthor ? a->name : s->name, argv[i+1]);
-        else if (mystrcmp(argv[i], "-genre") == 0)
+        if (mystrcmp(argv[i], "-name") == 0){
+            if (isAuthor){
+                a->name = malloc(mystrlen(argv[i+1]) + 1);
+                mystrcpy(a->name, argv[i+1]);
+            }
+            else{
+                s->name = malloc(mystrlen(argv[i+1]) + 1);
+                mystrcpy(s->name, argv[i+1]);
+            }
+        }
+        else if (mystrcmp(argv[i], "-type") == 0){
+            s->type = malloc(mystrlen(argv[i+1]) + 1);
             mystrcpy(s->type, argv[i+1]);
+        }
         else if (mystrcmp(argv[i], "-date") == 0) {
             if (isAuthor) {
                 a->birthdate.day = myatoi(argv[i+1]);
@@ -79,15 +101,39 @@ int main(int argc, char **argv){
         else if (mystrcmp(argv[i], "-height") == 0) s->height = myatoi(argv[i+1]);
         else if (mystrcmp(argv[i], "-width") == 0) s->width = myatoi(argv[i+1]);
         else if (mystrcmp(argv[i], "-stock") == 0) s->stock = myatoi(argv[i+1]);
-        else if (mystrcmp(argv[i], "-author") == 0) mystrcpy(s->author, argv[i+1]);
-        else if (mystrcmp(argv[i], "-street") == 0) mystrcpy(a->street, argv[i+1]);
-        else if (mystrcmp(argv[i], "-mail") == 0) mystrcpy(a->mail, argv[i+1]);
-        else if (mystrcmp(argv[i], "-site") == 0) mystrcpy(a->website, argv[i+1]);
-        else if (mystrcmp(argv[i], "-phone") == 0) mystrcpy(a->phone, argv[i+1]);
-        else if (mystrcmp(argv[i], "-country") == 0) mystrcpy(a->country, argv[i+1]);
+        else if (mystrcmp(argv[i], "-author") == 0){
+            s->author = malloc(mystrlen(argv[i+1]) + 1);
+            mystrcpy(s->author, argv[i+1]);
+        }
+        else if (mystrcmp(argv[i], "-street") == 0){
+            a->street = malloc(mystrlen(argv[i+1]) + 1);
+            mystrcpy(a->street, argv[i+1]);
+        }
+        else if (mystrcmp(argv[i], "-mail") == 0){
+            a->mail = malloc(mystrlen(argv[i+1]) + 1);
+            mystrcpy(a->mail, argv[i+1]);
+        }
+        else if (mystrcmp(argv[i], "-site") == 0){
+            a->website = malloc(mystrlen(argv[i+1]) + 1);
+            mystrcpy(a->website, argv[i+1]);
+        }
+        else if (mystrcmp(argv[i], "-phone") == 0){
+            a->phone = malloc(mystrlen(argv[i+1]) + 1);
+            mystrcpy(a->phone, argv[i+1]);
+        }
+        else if (mystrcmp(argv[i], "-country") == 0){
+            a->country = malloc(mystrlen(argv[i+1]) + 1);
+            mystrcpy(a->country, argv[i+1]);
+        }
 
-        else if (mystrcmp(argv[i], "--add") == 0)
-            isAuthor ? append_author(a) : append_item(s);
+        else if (mystrcmp(argv[i], "--add") == 0){
+            if (isAuthor){
+                append_author(a);
+            }
+            else{
+                append_item(s);
+            }
+        }
 
         else if (mystrcmp(argv[i], "--rm") == 0) {
             if (isAuthor)
@@ -100,10 +146,21 @@ int main(int argc, char **argv){
                 print_entry(AUTHORS_FILE, "Author:", argv[i+1]);
             else
                 print_entry(DATABASE_FILE, "Name:", argv[i+1]);
-}
+        }
     }
 
+    free(s->name);
+    free(s->type);
+    free(s->author);
     free(s);
+
+    free(a->name);
+    free(a->street);
+    free(a->mail);
+    free(a->website);
+    free(a->phone);
+    free(a->country);
     free(a);
+    
     return 0;
 }
