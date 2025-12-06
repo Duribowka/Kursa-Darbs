faf5230fea76294c46f54ff1d9b4f23f0b73da0c
Duribowka <s.boronin08@gmail.com>
2025-12-06 08:03:50 +0000

    Fixed critical core dumped bug!

--- main.c
+++ main.c
@@ -75,6 +75,9 @@ int main(int argc, char **argv){
         else if (mystrcmp(argv[i], "--item") == 0) isAuthor = 0;
 
         if (mystrcmp(argv[i], "-name") == 0){
+            if (i + 1 >= argc){
+                continue;
+            }
             if (isAuthor){
                 a->name = malloc(mystrlen(argv[i+1]) + 1);
                 mystrcpy(a->name, argv[i+1]);
@@ -85,46 +88,95 @@ int main(int argc, char **argv){
             }
         }
         else if (mystrcmp(argv[i], "-type") == 0){
+            if (i + 1 >= argc){
+                continue;
+            }
             s->type = malloc(mystrlen(argv[i+1]) + 1);
             mystrcpy(s->type, argv[i+1]);
         }
-        else if (mystrcmp(argv[i], "-date") == 0) {
-            if (isAuthor) {
+        else if (mystrcmp(argv[i], "-date") == 0){
+            if (i + 1 >= argc){
+                continue;
+            }
+            if (isAuthor){
                 a->birthdate.day = myatoi(argv[i+1]);
                 a->birthdate.month = myatoi(argv[i+2]);
                 a->birthdate.year = myatoi(argv[i+3]);
-            } else {
+            } else{
                 s->date.day = myatoi(argv[i+1]);
                 s->date.month = myatoi(argv[i+2]);
                 s->date.year = myatoi(argv[i+3]);
             }
         }
-        else if (mystrcmp(argv[i], "-price") == 0) s->price = myatoi(argv[i+1]);
-        else if (mystrcmp(argv[i], "-weight") == 0) s->weight = myatoi(argv[i+1]);
-        else if (mystrcmp(argv[i], "-height") == 0) s->height = myatoi(argv[i+1]);
-        else if (mystrcmp(argv[i], "-width") == 0) s->width = myatoi(argv[i+1]);
-        else if (mystrcmp(argv[i], "-stock") == 0) s->stock = myatoi(argv[i+1]);
+        else if (mystrcmp(argv[i], "-price") == 0){
+            if (i + 1 >= argc){
+                continue;
+            }
+            s->price = myatoi(argv[i+1]);
+        }
+        else if (mystrcmp(argv[i], "-weight") == 0){
+            if (i + 1 >= argc){
+                continue;
+            }
+            s->weight = myatoi(argv[i+1]);
+        }
+        else if (mystrcmp(argv[i], "-height") == 0){
+            if (i + 1 >= argc){
+                continue;
+            }
+            s->height = myatoi(argv[i+1]);
+        }
+        else if (mystrcmp(argv[i], "-width") == 0){
+            if (i + 1 >= argc){
+                continue;
+            }
+            s->width = myatoi(argv[i+1]);
+        }
+        else if (mystrcmp(argv[i], "-stock") == 0){
+            if (i + 1 >= argc){
+                continue;
+            }
+            s->stock = myatoi(argv[i+1]);
+        }
         else if (mystrcmp(argv[i], "-author") == 0){
+            if (i + 1 >= argc){
+                continue;
+            }
             s->author = malloc(mystrlen(argv[i+1]) + 1);
             mystrcpy(s->author, argv[i+1]);
         }
         else if (mystrcmp(argv[i], "-street") == 0){
+            if (i + 1 >= argc){
+                continue;
+            }
             a->street = malloc(mystrlen(argv[i+1]) + 1);
             mystrcpy(a->street, argv[i+1]);
         }
         else if (mystrcmp(argv[i], "-mail") == 0){
+            if (i + 1 >= argc){
+                continue;
+            }
             a->mail = malloc(mystrlen(argv[i+1]) + 1);
             mystrcpy(a->mail, argv[i+1]);
         }
         else if (mystrcmp(argv[i], "-site") == 0){
+            if (i + 1 >= argc){
+                continue;
+            }
             a->website = malloc(mystrlen(argv[i+1]) + 1);
             mystrcpy(a->website, argv[i+1]);
         }
         else if (mystrcmp(argv[i], "-phone") == 0){
+            if (i + 1 >= argc){
+                continue;
+            }
             a->phone = malloc(mystrlen(argv[i+1]) + 1);
             mystrcpy(a->phone, argv[i+1]);
         }
         else if (mystrcmp(argv[i], "-country") == 0){
+            if (i + 1 >= argc){
+                continue;
+            }
             a->country = malloc(mystrlen(argv[i+1]) + 1);
             mystrcpy(a->country, argv[i+1]);
         }
@@ -164,6 +216,9 @@ int main(int argc, char **argv){
             }
         }
         else if (mystrcmp(argv[i], "--edit") == 0){
+            if (i + 1 >= argc){
+                continue;
+            }
             const char *target = argv[i + 1];
             const char *category = argv[i + 2];
             const char *newValue = argv[i + 3];
@@ -174,19 +229,6 @@ int main(int argc, char **argv){
                 edit_entry(DATABASE_FILE, "Name:", target, category, newValue);
         }
     }
-
-    free(s->name);
-    free(s->type);
-    free(s->author);
-    free(s);
-
-    free(a->name);
-    free(a->street);
-    free(a->mail);
-    free(a->website);
-    free(a->phone);
-    free(a->country);
-    free(a);
     
     return 0;
 }
